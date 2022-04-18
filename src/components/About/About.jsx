8e2ce3ef412c458a97240aa2bc571aca502ab30c
ZIHAD22 <mdzihad2244@gmail.com>
2022-04-18 23:31:38 +0600

    add about page

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -2,8 +2,23 @@ import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h1>this is About 2</h1>
+    <div className="row w-100">
+      <div className="col-md-6 mx-auto mt-5">
+        <div class="row shadow p-5 g-3">
+          <div className="col-md-8 d-flex align-items-center">
+            <div>
+              <h1>Name: MD.ZIHAD</h1>
+              <strong>
+                <h5>From: Bangladesh</h5>
+              </strong>
+              <p>
+                Hi I am MD.ZIHAD. I am a passionate programmer. I feel comfort
+                in programming. My goal is software engineer.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
